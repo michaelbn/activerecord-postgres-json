3e2d346340197b7ec7f22d7fa5537b0b28e7ccb0
mpasternak <michal.pasternak@in4mates.pl>
2013-01-29 16:43:08 +0100

    Initial commit

---
+++ lib/activerecord-postgres-json.rb
@@ -0,0 +1,9 @@
+require "activerecord-postgres-json/version"
+
+module Activerecord
+  module Postgres
+    module Json
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/activerecord-postgres-json/version.rb
@@ -0,0 +1,7 @@
+module Activerecord
+  module Postgres
+    module Json
+      VERSION = "0.0.1"
+    end
+  end
+end
